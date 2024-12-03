2f9b4b6eaaf7b676064de79d6e11eba44101cd0d
wanseopchoi <wanseop.choi@lge.com>
2024-12-03 14:03:47 +0900

    raw dataframe_NC + tVOC label_4 == 1 (유증기) Cook == 1,3 (삼겹살, 고등어)

--- PreProcess.py
+++ PreProcess.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 23 11:23:45 2024
-
-@author: younjae.lee
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Tue Feb 20 13:50:17 2024
 
 @author: younjae.lee
@@ -16,47 +9,17 @@ import numpy as np
 import pandas as pd
 import os
 
-
-# from sklearn.preprocessing import MinMaxScaler
-# from sklearn.preprocessing import StandardScaler
-# import seaborn as sns
-# import time
-# from datetime import datetime
-# from scipy import interpolate
-# import scipy.interpolate as spi
-# from scipy.signal import find_peaks
-# from csaps import csaps
-# from scipy.signal import butter, lfilter
-# import statistics
-# from scipy import signal
-# from scipy.fft import fftshift
-# from sklearn.cluster import KMeans
-
-
 def FeatureRename(data, Name):
-    """
-        데이터프레임의 열 이름을 변경하는 함수
-        :param data: 입력 데이터프레임
-        :param Name: 새로운 열 이름 리스트
-        :return: 열 이름이 변경된 데이터프레임
-        """
     data.columns = Name
 
     return data
 
-
 def CalBase(data):
-    """
-        데이터프레임의 기본 통계 값을 계산하는 함수
-        :param data: 입력 데이터프레임
-        :return: 기본 통계 값을 포함하는 시리즈
-        """
     Base = data.mean(numeric_only=True)
-    Base['PM10'] = data['PM10'].min(numeric_only=True)
-    Base['SGP4x_3'] = data['SGP4x_3'].max(numeric_only=True)
+    Base['PM10'] = data.PM10.min(numeric_only=True)
+    Base['SGP4x_3'] = data.SGP4x_3.max(numeric_only=True)
 
     return Base
-
 
 def RawDataInterpolation(RawData):
     TIndex = pd.to_datetime(RawData['datetime'])
@@ -65,27 +28,27 @@ def RawDataInterpolation(RawData):
     df = df.astype(float)
 
     rdf = df.resample(rule='S').mean()
-    Int_df = rdf.interpolate(method='cubic')
+    Int_df = rdf.interpolate('cubic')
 
     return Int_df
-
 
 def EventDetection(label_data):
     Event = pd.DataFrame()
     for i in range(len(label_data)):
         if i > 0:
-            print(label_data['label_4'][i - 1], label_data['label_4'][i])
-            if label_data['label_4'][i - 1] != label_data['label_4'][i]:
+            print(label_data.label_4[i - 1], label_data.label_4[i])
+            if label_data.label_4[i - 1] != label_data.label_4[i]:
                 Event = pd.concat([Event, label_data.iloc[i, :]], axis=1)
     Event = Event.transpose()
     return Event
-
 
 def MakeEvent_df(Event, Int_df):
     EIndex = pd.to_datetime(Event['datetime'])
     df = pd.DataFrame(Event.values, index=EIndex, columns=Event.columns)
     df = df.drop('datetime', axis=1)
-    df = df.label_4.astype(float)
+    df = df.drop('time_end', axis=1)
+    # df=df.label_4.astype(float)
+    df = df.astype(float)
 
     StartTime = Int_df.index[0]
     EndTime = Int_df.index[len(Int_df) - 1]
@@ -97,8 +60,7 @@ def MakeEvent_df(Event, Int_df):
 
     return rdf, StartTime, EndTime
 
-
-def Cal_Sdata(RawData):
+def Cal_Sdata(RawData, FeatureList):
     data = RawData.copy()
     data['R_SGP40_0'] = 11.0 * np.exp(data['SGP4x_0'] / 2871.0)
     data['R_SGP40_1'] = 11.0 * np.exp(data['SGP4x_1'] / 2871.0)
@@ -117,11 +79,14 @@ def Cal_Sdata(RawData):
 
     data['PM10'] = data['PM10'] / 100
 
-    feature = ['datetime', 'PM10', 'S_SGP40_0', 'S_SGP40_1', 'S_SGP40_2', 'S_SGP40_3']
+    feature = ['datetime', 'rPM10', 'rPM25', 'rPM1', 'PM10', 'PM25', 'PM1', 'OPT_PM01', 'OPT_PM02', 'OPT_PM04',
+               'OPT_PM10', 'Temp', 'Humi',
+               'S_SGP40_0', 'S_SGP40_1', 'S_SGP40_2', 'S_SGP40_3', 'OPT_NCu5', 'OPT_NC01', 'OPT_NC02', 'OPT_NC04',
+               'OPT_NC10',
+               ' NC0_5', 'NC1_0', ' NC2_5', ' NC4_0', ' NC10_0', 'final_output_tVOC']
     data = data[feature]
 
     return data, Base
-
 
 def add_Ratio(data):
     data['Ratio_SGP40_0'] = data['S_SGP40_0'] / data['PM10']
@@ -130,7 +95,6 @@ def add_Ratio(data):
     data['Ratio_SGP40_3'] = data['S_SGP40_3'] / data['PM10']
 
     return data
-
 
 def add_Diff(data):
     n = 60
@@ -143,32 +107,42 @@ def add_Diff(data):
 
     return data
 
-
 def Process(data, label):
     data['datetime'] = data.DATE + ' ' + data.TIME
     label['datetime'] = label.date_start + ' ' + label.time_start
 
-    feature_d = ['datetime', ' P-PM10', ' unMEMS_SGP4x_Data0]0]', ' unMEMS_SGP4x_Data0]1]', ' unMEMS_SGP4x_Data0]2]',
-                 ' unMEMS_SGP4x_Data0]3]']
-    feature_l = ['datetime', 'time_end', 'label_4']
+    feature_d = ['datetime', 'PM10 [ug/m3]', 'PM2.5 [ug/m3]', 'PM1 [ug/m3]', ' P-PM10', ' P-PM2.5', ' P-PM1.0'
+        , 'OPT_PM01', 'OPT_PM02', 'OPT_PM04', 'OPT_PM10', ' fTemperature', ' fHumid',
+                 ' unMEMS_SGP4x_Data0]0]', ' unMEMS_SGP4x_Data0]1]', ' unMEMS_SGP4x_Data0]2]', ' unMEMS_SGP4x_Data0]3]',
+                 'OPT_NCu5', 'OPT_NC01', 'OPT_NC02', 'OPT_NC04', 'OPT_NC10', ' NC0_5', 'NC1_0', ' NC2_5', ' NC4_0',
+                 ' NC10_0',
+                 'final_output_tVOC']
+    feature_l = ['datetime', 'time_end', 'label_4', 'cooks_code', 'vent']
     RawData = data[feature_d]
     label_data = label[feature_l]
 
-    RawData = FeatureRename(RawData, ['datetime', 'PM10', 'SGP4x_0', 'SGP4x_1', 'SGP4x_2', 'SGP4x_3'])
-    SData, Base = Cal_Sdata(RawData)
+    FeatureList = ['datetime', 'rPM10', 'rPM25', 'rPM1', 'PM10', 'PM25', 'PM1',
+                   'OPT_PM01', 'OPT_PM02', 'OPT_PM04', 'OPT_PM10', 'Temp', 'Humi',
+                   'SGP4x_0', 'SGP4x_1', 'SGP4x_2', 'SGP4x_3', 'OPT_NCu5', 'OPT_NC01', 'OPT_NC02', 'OPT_NC04',
+                   'OPT_NC10',
+                   ' NC0_5', 'NC1_0', ' NC2_5', ' NC4_0', ' NC10_0', 'final_output_tVOC']
+    RawData = FeatureRename(RawData, FeatureList)
+    SData, Base = Cal_Sdata(RawData, FeatureList)
     SData = add_Ratio(SData)
-    SData = add_Diff(SData)
+    # SData=add_Diff(SData)
 
     Int_df = RawDataInterpolation(SData)
     Event = EventDetection(label_data)
     Event_df, StartTime, EndTime = MakeEvent_df(Event, Int_df)
 
-    Int_df['class'] = Event_df
+    Int_df['label_4'] = Event_df['label_4']
+    Int_df['Cook'] = Event_df['cooks_code']
+    Int_df['Vent'] = Event_df['vent']
+
     Int_df.plot()
     plt.title(Event_df.index[0])
 
-    return Int_df, Event_df
-
+    return Int_df, Event_df, Event
 
 path = './All/'
 file_list = os.listdir(path)
@@ -194,9 +168,12 @@ for i in range(len(RawData)):
     data = RawData[i]
     label = Label[i]
 
-    Int_df, Event_df = Process(data, label)
+    Int_df, Event_df, Event = Process(data, label)
     All_df = pd.concat([All_df, Int_df])
 
-All_df.to_csv('All.csv')
+filtered_All_df = All_df[(All_df['label_4'] == 1) & (All_df['Cook'].isin([1, 3]))]  # 유증기 요리만 추출, 삼겹살과 고등어만
+print(filtered_All_df)
 
+filtered_All_df.to_csv('All.csv')
 
+# All_df.to_csv('All_10.csv')
